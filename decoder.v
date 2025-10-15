968cc5fa42f48a0fa4cb1ff55ceaf6f29bdd9846
Tyler Smith <tylertms@icloud.com>
2025-10-15 13:26:02 -0400

    Adjust comments

--- decoder.v
+++ decoder.v
@@ -5,6 +5,7 @@ module decoder (
 // Defined on page 130 of:
 // https://github.com/riscv/riscv-isa-manual/releases/download/Ratified-IMAFDQC/riscv-spec-20191213.pdf
 
+// Decode instruction type
 wire is_alu_reg = (instr[6:0] == 7'b0110011); // reg <= reg op reg
 wire is_alu_imm = (instr[6:0] == 7'b0010011); // reg <= reg op imm
 wire is_branch  = (instr[6:0] == 7'b1100011); // if (reg op reg) pc <= pc + imm
@@ -16,13 +17,16 @@ wire is_load    = (instr[6:0] == 7'b0000011); // reg <= mem[reg + imm]
 wire is_store   = (instr[6:0] == 7'b0100011); // mem[reg + imm] <= reg
 wire is_system  = (instr[6:0] == 7'b1110011); // ...
 
-wire [4:0] rs1_id = instr[19:15]; // Source Register 1 for R/I/S/B type instructions
-wire [4:0] rs2_id = instr[24:20]; // Source Register 2 for R/S/B type instructions
-wire [4:0] rd_id  = instr[11:7]; // Destination Register for R/I/U/J type instructions
+// Decode source/destination registers
+wire [4:0] rs1_id = instr[19:15];
+wire [4:0] rs2_id = instr[24:20];
+wire [4:0] rd_id  = instr[11:7];
 
+// Decode instruction funct within instruction types
 wire [3:0] funct3 = instr[14:12];
 wire [6:0] funct7 = instr[31:25];
 
+// Decode immediates for different instruction types
 wire [31:0] imm_i = {{21{instr[31]}}, instr[30:20]};
 wire [31:0] imm_s = {{21{instr[31]}}, instr[30:25], instr[11:7]};
 wire [31:0] imm_b = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
