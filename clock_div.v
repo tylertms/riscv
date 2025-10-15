f103ce2b03a5a6bdd390e3ac73c44e24c57c4674
Tyler Smith <tylertms@icloud.com>
2025-10-15 17:02:17 -0400

    Better clock div (72%)

--- clock_div.v
+++ clock_div.v
@@ -7,33 +7,52 @@ module clock_div #(
     output wire clk_out
 );
 
-reg A, B_pos, B_neg;
+// A and B toggle on rising and falling edges, respectively, when appropriate
+reg A, B;
+
+// The counter state is used to time the toggling of A and B at the beginning
+// and middle of the didived output clock, respectively.
 reg [WIDTH-1:0] counter;
-wire [WIDTH-1:0] next_counter = (counter == div_num) ? 0 : counter + 1;
+
+// If the divisor is even, only A can be used to toggle the output clock
+// as all toggles happen on rising edges. Thus, the counter maximum value
+// is half of what it would be otherwise.
+wire [WIDTH-1:0] reset_num = div_num >> div_num[0];
+
+// The next counter state either increments or resets back to 0 at the reset_num
+wire [WIDTH-1:0] next_counter = (counter == reset_num) ? 0 : counter + 1;
 
 always @(posedge clk_in or posedge reset) begin
+    // A/Counter reset logic
     if (reset) begin
-        counter <= 0;
-        A <= 0; B_pos <= 0;
-    end else begin
-        if (next_counter == div_num) 
+        A <= 0; counter <= 0;
+    end
+
+    else begin
+        // If the counter is at the maximum, toggle A
+        // This occurs at the start/end of a new divided clock
+        if (counter == reset_num)
             A <= ~A;
 
-        if (div_num[0] && next_counter == (div_num >> 1))
-            B_pos <= ~B_pos;
-
+        // Assign the next state
         counter <= next_counter;
     end
 end
 
 always @(negedge clk_in or posedge reset) begin
+    // B reset logic
     if (reset)
-        B_neg <= 0;
-        
-    else if (!div_num[0] && next_counter == (div_num >> 1))
-        B_neg <= ~B_neg;
+        B <= 0;
+
+    // If the divisor is odd (B is XOR'ed with A), then toggle
+    // B on the falling edge halfway through the divided clock cycle
+    else if (!div_num[0] && counter == (div_num >> 1))
+        B <= ~B;
 end
 
-assign clk_out = A ^ B_pos ^ B_neg;
+// By XORing A and B, we can either produce an output using only A
+// with even divisors and rising edges, or toggle the output clock
+// on a falling edge by toggling B
+assign clk_out = A ^ B;
 
 endmodule
