e35480ce8059a80fb324332694d950416377b9c5
Tyler Smith <tylertms@icloud.com>
2025-10-29 23:41:39 -0400

    Add more utilities

--- programs/include/vec3.h
+++ programs/include/vec3.h
@@ -2,17 +2,23 @@
 #include "fxp.h"
 #include "math.h"
 
-#define FXP_COLOR_SCALE ((fxp32_t)16711680)
-
 typedef struct _vec3 {
     fxp32_t x, y, z;
 } _vec3;
+
+static inline _vec3 vec3_add_vec3(_vec3 a, _vec3 b) {
+    return (_vec3){
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .z = a.z + b.z
+    };
+}
 
 static inline _vec3 vec3_sub_vec3(_vec3 a, _vec3 b) {
     return (_vec3){
         .x = a.x - b.x,
         .y = a.y - b.y,
-        .z = a.z - b.z,
+        .z = a.z - b.z
     };
 }
 
@@ -20,7 +26,7 @@ static inline _vec3 vec3_mul_fxp(_vec3 v, fxp32_t s) {
     return (_vec3){
         .x = fxp_mul(v.x, s),
         .y = fxp_mul(v.y, s),
-        .z = fxp_mul(v.z, s),
+        .z = fxp_mul(v.z, s)
     };
 }
 
@@ -28,7 +34,47 @@ static inline _vec3 vec3_div_fxp(_vec3 v, fxp32_t s) {
     return (_vec3){
         .x = fxp_div(v.x, s),
         .y = fxp_div(v.y, s),
-        .z = fxp_div(v.z, s),
+        .z = fxp_div(v.z, s)
+    };
+}
+
+static inline _vec3 vec3_mul_vec3(_vec3 a, _vec3 b) {
+    return (_vec3){
+        .x = fxp_mul(a.x, b.x),
+        .y = fxp_mul(a.y, b.y),
+        .z = fxp_mul(a.z, b.z)
+    };
+}
+
+static inline _vec3 vec3_div_vec3(_vec3 a, _vec3 b) {
+    return (_vec3){
+        .x = fxp_div(a.x, b.x),
+        .y = fxp_div(a.y, b.y),
+        .z = fxp_div(a.z, b.z)
+    };
+}
+
+static inline _vec3 vec3_mul_int32(_vec3 v, int32_t s) {
+    return (_vec3){
+        .x = v.x * s,
+        .y = v.y * s,
+        .z = v.z * s
+    };
+}
+
+static inline _vec3 vec3_div_int32(_vec3 v, int32_t s) {
+    return (_vec3){
+        .x = v.x / s,
+        .y = v.y / s,
+        .z = v.z / s
+    };
+}
+
+static inline _vec3 vec3_neg(_vec3 v) {
+    return (_vec3){
+        .x = -v.x,
+        .y = -v.y,
+        .z = -v.z
     };
 }
 
@@ -46,4 +92,12 @@ static inline fxp32_t vec3_len(_vec3 v) {
 
 static inline _vec3 vec3_normalize(_vec3 v) {
     return vec3_div_fxp(v, vec3_len(v));
+}
+
+static inline _vec3 vec3_unit_to_uniform01(_vec3 v) {
+    return (_vec3){
+        .x = (v.x + FXP_ONE) >> 1,
+        .y = (v.y + FXP_ONE) >> 1,
+        .z = (v.z + FXP_ONE) >> 1
+    };
 }
